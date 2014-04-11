4a7a2abd2e4bb9652caa9e2330b4c43bded3e9ef
tenten0213 <takehito.0213@gmail.com>
2014-04-11 13:02:28 +0900

    assertThatの引数の順番を修正

--- src/test/java/chapter1/Chapter1_1Test.java
+++ src/test/java/chapter1/Chapter1_1Test.java
@@ -8,18 +8,18 @@ import static org.junit.Assert.assertThat;
 public class Chapter1_1Test {
     @Test
     public void 同一の文字が含まれない場合は真となること() {
-        assertThat(true, is(Chapter1_1.isUnique("hoge")));
+        assertThat(Chapter1_1.isUnique("hoge"), is(true));
     }
     @Test
     public void 文字列が１文字の場合は真となること() {
-        assertThat(true, is(Chapter1_1.isUnique("a")));
+        assertThat(Chapter1_1.isUnique("a"), is(true));
     }
     @Test
     public void 同一の文字が含まれる場合は偽となること() {
-        assertThat(false, is(Chapter1_1.isUnique("abcdefga")));
+        assertThat(Chapter1_1.isUnique("abcdefga"), is(false));
     }
     @Test
     public void マルチバイトで同一の文字が含まれる場合は偽となること() {
-        assertThat(false, is(Chapter1_1.isUnique("あいうえおあ")));
+        assertThat(Chapter1_1.isUnique("あいうえおあ"), is(false));
     }
 }
--- src/test/java/chapter1/Chapter1_3Test.java
+++ src/test/java/chapter1/Chapter1_3Test.java
@@ -8,14 +8,14 @@ import static org.junit.Assert.*;
 public class Chapter1_3Test {
     @Test
     public void 与えられた２つの文字列が異なる長さの場合は偽であること() {
-        assertThat(false, is(Chapter1_3.isEqualAfterSorted("god", "doggy")));
+        assertThat(Chapter1_3.isEqualAfterSorted("god", "doggy"), is(false));
     }
     @Test
     public void 与えられた２つの文字列が同一の文字で構成されている場合は真であること() {
-        assertThat(true, is(Chapter1_3.isEqualAfterSorted("god", "dog")));
+        assertThat(Chapter1_3.isEqualAfterSorted("god", "dog"), is(true));
     }
     @Test
     public void 与えられた２つの文字列が同一の文字で構成されていない場合は偽であること() {
-        assertThat(false, is(Chapter1_3.isEqualAfterSorted("god", "bog")));
+        assertThat(Chapter1_3.isEqualAfterSorted("god", "bog"), is(false));
     }
 }
