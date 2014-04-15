f5300876bde781daa8dae0d535ec6e5e8b033fe8
tenten0213 <takehito.0213@gmail.com>
2014-04-15 23:34:12 +0900

    CharBufferを使って実装

--- src/main/java/chapter1/Chapter1_4.java
+++ src/main/java/chapter1/Chapter1_4.java
@@ -1,7 +1,24 @@
 package chapter1;
 
+import java.nio.CharBuffer;
+import java.util.Arrays;
+
 public class Chapter1_4 {
-    public static String replaceSpaces(String target) {
-        return target.trim().replaceAll("\\s", "%20");
+    public static String replaceSpacesBuffer(Character[] chars, int length) {
+
+        int spaceCount = ((int) Arrays.asList(chars).stream()
+                .filter(s -> s.equals(' '))
+                .count());
+
+        CharBuffer buffer = CharBuffer.allocate(length + spaceCount * 2);
+        for(int i = 0; i < length; i++) {
+            if(chars[i] == ' ') {
+                buffer.put("%20");
+            } else {
+                buffer.put(chars[i]);
+            }
+        }
+        buffer.flip();
+        return buffer.toString();
     }
 }
