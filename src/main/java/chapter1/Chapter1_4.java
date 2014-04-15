2848b650e86f49fe081a357076d979240aff9e15
tenten0213 <takehito.0213@gmail.com>
2014-04-16 00:02:42 +0900

    変数を説明的にした:neckbeard:

--- src/main/java/chapter1/Chapter1_4.java
+++ src/main/java/chapter1/Chapter1_4.java
@@ -6,11 +6,11 @@ import java.util.Arrays;
 public class Chapter1_4 {
     public static String replaceSpacesBuffer(Character[] chars, int length) {
 
-        int spaceCount = ((int) Arrays.asList(chars).stream()
+        int encodedCharsLength = length + ((int) Arrays.asList(chars).stream()
                 .filter(s -> s.equals(' '))
-                .count());
+                .count()) * 2;
 
-        CharBuffer buffer = CharBuffer.allocate(length + spaceCount * 2);
+        CharBuffer buffer = CharBuffer.allocate(encodedCharsLength);
         for(int i = 0; i < length; i++) {
             if(chars[i] == ' ') {
                 buffer.put("%20");
